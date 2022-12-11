1cbacb6c3c81c38bec4022619d316694327215c8
Maycon Wallas <maycon.wallas.balbino@gmail.com>
2022-12-11 11:49:51 -0300

    testando
    apenas alguns testes.

--- MeusProjetosJavaScript/_javascript/_javascript.js
+++ MeusProjetosJavaScript/_javascript/_javascript.js
@@ -10,9 +10,11 @@
     */
 //window.alert("Olá, Mundo!")
  
-// window.confirm("Está gostando de javascript??")       
+ //window.confirm("Está gostando de javascript??")       
 
  var numero1 = Number( window.prompt("Digite um numero."))
  var nuemro2 = Number(window.prompt("Digite outro numero."))
  var resultado1 = numero1 + nuemro2
- window.alert(`A soma dos seus numero é: ${resultado1}`) // o String esta apensas
+ window.alert(`A soma dos seus numero é: ${resultado1}`)
+ // o String esta apensas
+             
